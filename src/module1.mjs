cc162f078bed4bc730de3b6c012944bde9e3f99d
ayush23719 <91633771+ayush23719@users.noreply.github.com>
2022-04-23 16:01:17 +0530

    Navbar (props)

---
+++ src/module1.mjs
@@ -0,0 +1,6 @@
+let a = [20,30,40,50,60];
+const b = "Ayush";
+const c = "Aviral";
+
+export default b;
+export {a};
---
+++ src/module2.mjs
@@ -0,0 +1,4 @@
+import ay,{a} from './module1.mjs';
+
+console.log(ay);
+console.log(a);
