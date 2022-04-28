2caa672514781f57642d076dfdb6354bdf92979a
ayush23719 <91633771+ayush23719@users.noreply.github.com>
2022-04-29 00:11:02 +0530

    Dark Mode

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,6 +1,27 @@
 import React,{useState} from 'react'
 
 export default function TextForm(props) {
+  const [myStyle,setMyStyle] = useState({
+    color : 'black',
+    backgroundColor : 'white'
+  })
+  const [btnText,setBtnText] = useState("Enable Dark Mode");
+  const toggleStyle = ()=>{
+    if(myStyle.color == 'white'){
+      setMyStyle({
+        color: 'black',
+        backgroundColor: 'white'
+      })
+      setBtnText("Enable Dark Mode");
+    }
+    else{
+      setMyStyle({
+        color: 'white',
+        backgroundColor: '#212529'
+      })
+      setBtnText("Disable Dark Mode")
+    }
+  }
     const [text,setText] = useState('');
     const handleUpClick = ()=>{
         console.log('Uppercase Clicked ' + text);
@@ -26,7 +47,7 @@ export default function TextForm(props) {
     
   return (
     <>
-    <div className="container">
+    <div className="container my-5" style = {myStyle}>
         <h1>{props.heading}</h1>
   <div className="mb-3">
     <textarea className="form-control" id="myBox" rows="8" value = {text} onChange = {handleOnChange}></textarea>
@@ -38,6 +59,10 @@ export default function TextForm(props) {
   <path d="M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.483 5.483 0 0 1 11.025 8a5.483 5.483 0 0 1-1.61 3.89l.706.706z"/>
   <path d="M10.025 8a4.486 4.486 0 0 1-1.318 3.182L8 10.475A3.489 3.489 0 0 0 9.025 8c0-.966-.392-1.841-1.025-2.475l.707-.707A4.486 4.486 0 0 1 10.025 8zM7 4a.5.5 0 0 0-.812-.39L3.825 5.5H1.5A.5.5 0 0 0 1 6v4a.5.5 0 0 0 .5.5h2.325l2.363 1.89A.5.5 0 0 0 7 12V4zM4.312 6.39 6 5.04v5.92L4.312 9.61A.5.5 0 0 0 4 9.5H2v-3h2a.5.5 0 0 0 .312-.11z"/>
 </svg></button>
+<div class="form-check form-switch mx-2 my-2">
+  <input class="form-check-input" type="checkbox" role="switch" id="darkMode" onClick={toggleStyle} />
+  <label class="form-check-label" for="darkMode">{btnText}</label>
+</div>
   </div>
   <div className="container">
     <h2>Your text summary</h2>
