d7bfd33521313a9990e542969cf772de67cb5ee3
ayush23719 <91633771+ayush23719@users.noreply.github.com>
2022-05-06 02:44:11 +0530

    git push

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function About(props) {
   return (
-    <div className="Container"><div class="accordion" id="accordionExample">
+    <div className="Container" style={props.myStyle}><div class="accordion" id="accordionExample">
     <div class="accordion-item" >
       <h2 class="accordion-header" id="headingOne">
         <button  class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
