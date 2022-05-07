66745006dcada9ffea60e12a0ce6b2a73e5d617f
ayush23719 <91633771+ayush23719@users.noreply.github.com>
2022-05-08 02:56:24 +0530

    console.log

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,16 +6,16 @@ export default function TextForm(props) {
         console.log('Uppercase Clicked ' + text);
         let newText = text.toUpperCase();
         setText(newText);
-        props.showAlert("Converted to uppercase","success");
+        props.showAlert("Converted to uppercase!","success");
     }
     const handleLowClick = ()=>{
       console.log('Lowercase Clicked ' + text);
       let newText = text.toLowerCase();
       setText(newText);
-      props.showAlert("Converted to lowercase","success");
+      props.showAlert("Converted to lowercase!","success");
   }
     const handleOnChange = (event)=>{
-        console.log('You clicked handleOnChange ');
+        console.log('You clicked handleOnChange! ');
         setText(event.target.value);
     }
     let wordCount  = text.split(" ").length;
